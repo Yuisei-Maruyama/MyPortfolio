2d2a9d9f4d231ab9e611700651ddd966ce929b72
Yuisei-Maruyama <yuisei.metro@gmail.com>
2021-09-23 02:23:40 +0900

    export対象追記

--- src/components.ts
+++ src/components.ts
@@ -1,2 +1,7 @@
 //  React Components
 export { default as Tooltip } from './components/Tooltip'
+export { default as ProfileCard } from './components/ProfileCard'
+export { default as History } from './components/History'
+export { default as Header } from './components/Header'
+export { default as ModeSwitch } from './components/ModeSwitch'
+export { default as ThemeProvider } from './components/ThemeProvider'
