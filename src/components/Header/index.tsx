bbca93e7d1e959161168edb49d0ffde838a260ae
Yuisei-Maruyama <yuisei.metro@gmail.com>
2021-11-19 00:47:31 +0900

    Drawerの実装

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
-import { ModeSwitch, ThemeProvider } from '@/components'
+import { ModeSwitch, ThemeProvider, Menu } from '@/components'
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,17 +22,23 @@ const useStyles = makeStyles((theme: Theme) =>
 const Header: React.FC = () => {
   const classes = useStyles()
   const [darkMode, setDarkMode] = useState<boolean>(false)
+  const [toggle, setToggle] = useState<boolean>(false)
+
+  const changeToggle = (toggle: boolean) => setToggle(!toggle)
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <Menu toggle={ toggle } changeToggle={() => changeToggle(toggle)} />
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => { setToggle(!toggle) }}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             MyPortfolio
           </Typography>
+          <IconButton edge="start" color="inherit" aria-label="github" onClick={() => { window.open('https://github.com/Yuisei-Maruyama/MyPortfolio', '_blank') }}><AiFillGithub /></IconButton>
+          <IconButton edge="start" color="inherit" aria-label="instagram" onClick={() => { window.open('https://www.instagram.com/y_metro/', '_blank') }}><AiFillInstagram /></IconButton>
           <ModeSwitch darkMode={darkMode} setDarkMode={setDarkMode}></ModeSwitch>
           <ThemeProvider mode={darkMode} />
         </Toolbar>
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Drawer } from '@material-ui/core'
+import { MenuList, MenuItem, ListItemIcon, ListItemText } from '@mui/material'
+import { FaPlane } from "react-icons/fa"
+import { AiTwotoneHome, AiOutlineBgColors } from 'react-icons/ai'
+import { BiCodeAlt } from 'react-icons/bi'
+import { useHistory, withRouter, RouteComponentProps } from 'react-router-dom'
+
+interface Props extends RouteComponentProps {
+  toggle: boolean,
+  changeToggle: (toggle: boolean) => void
+}
+
+interface Nav {
+  text: string,
+  icon: any,
+  nav: string
+}
+
+const Menu: React.FC<Props> = (props: Props) => {
+
+  const history = useHistory()
+
+  const itemList: Nav[] = [
+    { text: 'Home', icon: <AiTwotoneHome />, nav: '' },
+    { text: 'Technology', icon: <BiCodeAlt />, nav: 'technology' },
+    { text: 'Art', icon: <AiOutlineBgColors />, nav: 'art' },
+    { text: 'Travel', icon: <FaPlane />, nav: 'travel' }
+  ]
+
+  const handleDrawerToggle= (toggle: boolean) => {
+    props.changeToggle(!toggle) // Drawer の開閉状態を反転
+  }
+
+  const handleToPage = (item: { nav: string }, toggle: boolean) => {
+    history.push(`/${item.nav}`)
+    handleDrawerToggle(toggle)
+  }
+
+  return (
+    <Drawer open={props.toggle}>
+      <MenuList>
+        {itemList.map((item, index) => (
+          <MenuItem key={index} onClick={ () => handleToPage(item, props.toggle)}>
+            <ListItemIcon>
+              { item.icon }
+            </ListItemIcon>
+            <ListItemText primary={item.text} />
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Drawer>
+  )
+}
+
+export default withRouter(Menu)
+
