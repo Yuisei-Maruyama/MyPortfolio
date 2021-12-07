36b6a229d5f8e8a94307b0cf015f741e55e825ca
Yuisei-Maruyama <yuisei.metro@gmail.com>
2021-12-08 00:25:34 +0900

    update: componentsのエンドポイントのパスを修正

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+//  React Components
+export { default as Tooltip } from './Tooltip'
+export { default as ProfileCard } from './ProfileCard'
+export { default as History } from './History/History'
+export { default as Header } from './Header/Header'
+export { default as ModeSwitch } from './ModeSwitch/ModeSwitch'
+export { default as ThemeProvider } from './ThemeProvider'
+export { default as Circular } from './Circular/Circular'
+export { default as Menu } from './Menu/Menu'
+export { default as MessageArea } from './MessageArea/MessageArea'
+// export { default as Baffle } from './components/Baffle'
