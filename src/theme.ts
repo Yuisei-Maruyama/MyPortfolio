034bf8c22d0c99ea4daa6e3aee32d8b825525f2b
Yuisei-Maruyama <yuisei.metro@gmail.com>
2021-12-08 00:16:57 +0900

    add: テーマカラーの設定

---
+++ src/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+  typography: {
+    content: '"Titillium Web", sans-serif',
+    monospace: '"Source Code Pro", monospace'
+  },
+  color: {
+    background: '#001313',
+    section: '#001a1a',
+    border: '#06d8d7',
+    headings: '#00f8f8',
+    content: '#7efcf6',
+    active: '#d4f6fd',
+    error: '#ce003e'
+  },
+  breakpoints: {
+    tablet: 768,
+    tabletUp: '@media (min-width: 768px)',
+    tabletDown: '@media (max-width: 767px)'
+  }
+}
+
+export { theme }
