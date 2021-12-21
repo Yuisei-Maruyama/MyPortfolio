583be37d9e99ded58bcbca77b256179aeabe4ad3
Yuisei-Maruyama <yuisei.metro@gmail.com>
2021-12-21 11:02:13 +0900

    update: svgIconの修正

--- src/components/SvgIconSwitch/SvgIconSwitch.tsx
+++ src/components/SvgIconSwitch/SvgIconSwitch.tsx
@@ -18,7 +18,7 @@ const MaterialUISwitch = styled(Switch)(({ checked, checkedColor, unCheckColor }
       '& .MuiSwitch-thumb:before': {
         backgroundImage: `url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
           '#fff',
-        )}" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>')`,
+        )}" d="M20.37, 8.91L19.37, 10.64L7.24, 3.64L8.24, 1.91L11.28, 3.66L12.64, 3.29L16.97, 5.79L17.34, 7.16L20.37, 8.91M6, 19V7H11.07L18, 11V19A2, 2 0 0, 1 16, 21H8A2, 2 0 0, 1 6, 19Z" /></svg>')`,
         // )}" d=${checkedSvg} /></svg>')`,
       },
       '& + .MuiSwitch-track': {
@@ -42,7 +42,7 @@ const MaterialUISwitch = styled(Switch)(({ checked, checkedColor, unCheckColor }
       backgroundPosition: 'center',
       backgroundImage: `url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
           '#fff',
-      )}" d="M20.37, 8.91L19.37, 10.64L7.24, 3.64L8.24, 1.91L11.28, 3.66L12.64, 3.29L16.97, 5.79L17.34, 7.16L20.37, 8.91M6, 19V7H11.07L18, 11V19A2, 2 0 0, 1 16, 21H8A2, 2 0 0, 1 6, 19Z" /></svg>')`,
+      )}" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>')`,
       // )}" d=${unCheckedSvg} /></svg>')`,
     },
   },
