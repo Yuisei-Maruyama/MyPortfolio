c896703ade82dc8ab16aa963e8d51bddf6719d7a
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-01 02:04:05 +0900

    update: Profileカードの修正

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Card, CardContent, CardMedia, Typography } from '@material-ui/core'
+import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 
 const ProfileCard: React.FC = () => {
   return (
     <Card style={{ maxWidth: 400 }}>
-      <CardMedia component="img" image="assets/Profile.jpg" title="Contemplative Reptile" />
+      <CardMedia component="img" image="assets/Profile.jpg" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Yuisei Maruyama
@@ -13,10 +13,10 @@ const ProfileCard: React.FC = () => {
           Age: 24
         </Typography>
         <Typography variant="body2" color="textSecondary">
-          Job: FrontEndEngineer
+          Job: Front End Engineer
         </Typography>
         <Typography component={'div'} variant="body2" color="textSecondary">
-          Technologies:
+          Location: Tokyo/Japan
         </Typography>
       </CardContent>
     </Card>
