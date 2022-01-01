d77e181467b7c68ce541c956f13426092bdf594a
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-01 15:28:43 +0900

    update: レイアウト調整

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { rgba } from 'polished'
 
 const ProfileCard: React.FC = () => {
   return (
-    <Card style={{ maxWidth: 400 }}>
+    <Card style={{ maxWidth: 400, backgroundColor: rgba(0, 0, 0, 0.3), color: 'white' }}>
       <CardMedia component="img" image="assets/Profile.jpg" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Yuisei Maruyama
         </Typography>
-        <Typography variant="body2" color="textSecondary">
-          Age: 24
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          Job: Front-End Engineer
-        </Typography>
-        <Typography component={'div'} variant="body2" color="textSecondary">
+        <Typography variant="body2">Age: 24</Typography>
+        <Typography variant="body2">Job: Front-End Engineer</Typography>
+        <Typography component={'div'} variant="body2">
           Location: Tokyo/Japan
         </Typography>
       </CardContent>
