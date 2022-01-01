e0254ef7305e78f25a17ced14513b221228aa27f
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-02 07:39:16 +0900

    update: Material ui のデフォルトスタイルの書き換え

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import { Box, StepLabel, Stepper as BaseStepper, Step } from '@mui/material'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 
 type Props = {
   steps: string[]
   activeStep: number
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiStepLabel-label.Mui-active': { color: 'white' },
+      '& .MuiStepLabel-label.Mui-completed': { color: 'white' },
+    },
+  })
+)
+
 const Stepper: React.FC<Props> = (props: Props) => {
   const { steps, activeStep } = props
+
+  const classes = useStyles()
 
   return (
     <Box sx={{ width: '100%' }}>
       <BaseStepper activeStep={activeStep} alternativeLabel>
         {steps.map((label) => (
           <Step key={label}>
-            <StepLabel>{label}</StepLabel>
+            <StepLabel className={classes.root}>{label}</StepLabel>
           </Step>
         ))}
       </BaseStepper>
