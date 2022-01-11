b7e0f3b17ea6b1179f2bcb49e7db726ff521fecd
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-12 01:58:42 +0900

    update: Sliderの内容をコンポーネント切り出し

--- src/components/Circular/Circular.tsx
+++ src/components/Circular/Circular.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import { Box, Button } from '@material-ui/core'
-import { Chip, Avatar, Typography } from '@mui/material'
 import classes from './Circular.module.scss'
 import { Issue, Issues } from '@/types'
-import MarkdownPreview from '@uiw/react-markdown-preview'
-import { rgba } from 'polished'
+import { SliderContents } from '@/components'
 
 type Props = {
   length: number
@@ -54,25 +52,7 @@ const Circular: React.FC<Props> = (props: Props) => {
                 }}
                 className={classes.item}
               >
-                <Chip
-                  label={item.user.login}
-                  className={classes.chip}
-                  style={{ color: 'white', backgroundColor: rgba(0, 0, 0, 0.3), cursor: 'pointer' }}
-                  onClick={() => window.open(`https://github.com/Yuisei-Maruyama`, '_blank')}
-                  avatar={<Avatar alt={item.user.login} src={item.user.avatar_url} />}
-                />
-                <div className={classes.contents}>
-                  <Typography
-                    sx={{ fontSize: 13, fontWeight: 'bold', color: 'white', cursor: 'pointer' }}
-                    onClick={() => window.open(`${item.html_url}`, '_blank')}
-                  >
-                    {item.title}
-                  </Typography>
-                  <hr className={classes.divider} />
-                  <Typography sx={{ fontSize: 5 }} component="div">
-                    <MarkdownPreview source={item.body} style={{ fontSize: 11, fontWeight: 'bold' }} />
-                  </Typography>
-                </div>
+                <SliderContents item={item} />
               </div>
             )
           })}
