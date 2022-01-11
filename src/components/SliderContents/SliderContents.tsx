c5de13dc05a2077f89444f46c1fda2cbf5632dc0
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-12 02:30:06 +0900

    update: SlideContensの責務を明示的にする

--- src/components/SliderContents/SliderContents.tsx
+++ src/components/SliderContents/SliderContents.tsx
@@ -6,33 +6,43 @@ import classes from './SliderContents.module.scss'
 import { Issue } from '@/types'
 
 type Props = {
-  item: Issue
+  item: unknown,
+  specify?: string
 }
 
 const SliderContent: React.FC<Props> = (props: Props) => {
 
-  const { item } = props
+  const { item, specify } = props
 
   return (
     <>
-      <Chip
-        label={item.user.login}
-        style={{ color: 'white', backgroundColor: rgba(0, 0, 0, 0.3), cursor: 'pointer' }}
-        onClick={() => window.open(`https://github.com/Yuisei-Maruyama`, '_blank')}
-        avatar={<Avatar alt={item.user.login} src={item.user.avatar_url} />}
-      />
-      <div className={classes.contents}>
-        <Typography
-          sx={{ fontSize: 13, fontWeight: 'bold', color: 'white', cursor: 'pointer' }}
-          onClick={() => window.open(`${item.html_url}`, '_blank')}
-        >
-          {item.title}
-        </Typography>
-        <hr className={classes.divider} />
-        <Typography sx={{ fontSize: 5 }} component="div">
-          <MarkdownPreview source={item.body} style={{ fontSize: 11, fontWeight: 'bold' }} />
-        </Typography>
-      </div>
+      {
+        specify === 'github-todo'
+          ?
+            (
+              <>
+                <Chip
+                  label={(item as Issue).user.login}
+                  style={{ color: 'white', backgroundColor: rgba(0, 0, 0, 0.3), cursor: 'pointer' }}
+                  onClick={() => window.open(`https://github.com/Yuisei-Maruyama`, '_blank')}
+                  avatar={<Avatar alt={(item as Issue).user.login} src={(item as Issue).user.avatar_url} />}
+                />
+                <div className={classes.contents}>
+                  <Typography
+                    sx={{ fontSize: 13, fontWeight: 'bold', color: 'white', cursor: 'pointer' }}
+                    onClick={() => window.open(`${(item as Issue).html_url}`, '_blank')}
+                  >
+                    {(item as Issue).title}
+                  </Typography>
+                  <hr className={classes.divider} />
+                  <Typography sx={{ fontSize: 5 }} component="div">
+                    <MarkdownPreview source={(item as Issue).body} style={{ fontSize: 11, fontWeight: 'bold' }} />
+                  </Typography>
+                </div>
+              </>
+            )
+          : <></>
+        }
     </>
   )
 }
