9e9d280a22d3fdaac928649a50e720447bdc8ff4
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-13 00:40:10 +0900

    update: DOM要素名の修正

--- src/components/ComponentList/ComponentList.tsx
+++ src/components/ComponentList/ComponentList.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { CustomTreeItem } from '@/components/TreeViewer/TreeViewer'
+import TreeItem from '@mui/lab/TreeItem'
 
 const ComponentList: React.FC = () => {
   return (
     <>
-      <CustomTreeItem nodeId="1" label="Components">
-        <CustomTreeItem nodeId="2" label="Header" />
-        <CustomTreeItem nodeId="3" label="Table">
-          <CustomTreeItem nodeId="4" label="SkillTable" />
-          <CustomTreeItem nodeId="5" label="ResumeTable" />
-        </CustomTreeItem>
-        <CustomTreeItem nodeId="6" label="Footer" />
-      </CustomTreeItem>
+      <TreeItem nodeId="1" label="Components">
+        <TreeItem nodeId="2" label="Header" />
+        <TreeItem nodeId="3" label="Table">
+          <TreeItem nodeId="4" label="SkillTable" />
+          <TreeItem nodeId="5" label="ResumeTable" />
+        </TreeItem>
+        <TreeItem nodeId="6" label="Footer" />
+      </TreeItem>
     </>
   )
 }
--- src/components/DocumentList/DocumentList.tsx
+++ src/components/DocumentList/DocumentList.tsx
@@ -1,34 +1,34 @@
 import React from 'react'
-import { CustomTreeItem } from '@/components/TreeViewer/TreeViewer'
+import TreeItem from '@mui/lab/TreeItem'
 
 const DocumentList: React.FC = () => {
   return (
     <>
-      <CustomTreeItem nodeId="7" label="Documents">
-        <CustomTreeItem nodeId="20" label="Front-End">
-          <CustomTreeItem nodeId="8" label="README" />
-          <CustomTreeItem nodeId="9" label="React">
-            <CustomTreeItem nodeId="10" label="React Hooks">
-              <CustomTreeItem nodeId="11" label="useState" />
-              <CustomTreeItem nodeId="12" label="useEffect" />
-              <CustomTreeItem nodeId="13" label="useRef" />
-              <CustomTreeItem nodeId="14" label="useCallback" />
-            </CustomTreeItem>
-            <CustomTreeItem nodeId="15" label="react-router-dom" />
-            <CustomTreeItem nodeId="16" label="styled-components" />
-            <CustomTreeItem nodeId="17" label="Suspense" />
-            <CustomTreeItem nodeId="18" label="OAuth" />
-            <CustomTreeItem nodeId="19" label="Apollo-Client" />
-          </CustomTreeItem>
-          <CustomTreeItem nodeId="21" label="Vue">
-            <CustomTreeItem nodeId="22" label="Vue v3.x" />
-          </CustomTreeItem>
-        </CustomTreeItem>
-        <CustomTreeItem nodeId="23" label="Back-End">
-          <CustomTreeItem nodeId="24" label="Node.js" />
-          <CustomTreeItem nodeId="25" label="OpenAPI" />
-        </CustomTreeItem>
-      </CustomTreeItem>
+      <TreeItem nodeId="7" label="Documents">
+        <TreeItem nodeId="20" label="Front-End">
+          <TreeItem nodeId="8" label="README" />
+          <TreeItem nodeId="9" label="React">
+            <TreeItem nodeId="10" label="React Hooks">
+              <TreeItem nodeId="11" label="useState" />
+              <TreeItem nodeId="12" label="useEffect" />
+              <TreeItem nodeId="13" label="useRef" />
+              <TreeItem nodeId="14" label="useCallback" />
+            </TreeItem>
+            <TreeItem nodeId="15" label="react-router-dom" />
+            <TreeItem nodeId="16" label="styled-components" />
+            <TreeItem nodeId="17" label="Suspense" />
+            <TreeItem nodeId="18" label="OAuth" />
+            <TreeItem nodeId="19" label="Apollo-Client" />
+          </TreeItem>
+          <TreeItem nodeId="21" label="Vue">
+            <TreeItem nodeId="22" label="Vue v3.x" />
+          </TreeItem>
+        </TreeItem>
+        <TreeItem nodeId="23" label="Back-End">
+          <TreeItem nodeId="24" label="Node.js" />
+          <TreeItem nodeId="25" label="OpenAPI" />
+        </TreeItem>
+      </TreeItem>
     </>
   )
 }
