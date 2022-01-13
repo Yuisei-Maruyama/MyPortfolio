b5cde72ad14686b8bee4df0f112699423ecb9ccd
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-13 14:52:08 +0900

    update: OnClickに関わる関数を切り出し

--- src/components/DocumentList/DocumentList.tsx
+++ src/components/DocumentList/DocumentList.tsx
@@ -14,38 +14,42 @@ const DocumentList: React.FC = () => {
     history.push(`/document/${param}`)
   }
 
+  const previewDocument = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    handleToDocument(event).then(() => {
+      if (!toggle || !handleDrawerToggle) return
+      handleDrawerToggle(toggle)
+    })
+  }
+
 
   return (
     <>
       <TreeItem nodeId="7" label="Documents">
-        <TreeItem nodeId="20" label="Front-End">
-          <TreeItem nodeId="8" label="README" />
-          <TreeItem nodeId="9" label="React">
-            <TreeItem nodeId="26" label="Environment" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => handleToDocument(e).then(() => {
-              if (!toggle || !handleDrawerToggle) return
-              handleDrawerToggle(toggle)
-            })} />
-            <TreeItem nodeId="10" label="React Hooks">
-              <TreeItem nodeId="11" label="useState" />
-              <TreeItem nodeId="12" label="useEffect" />
-              <TreeItem nodeId="13" label="useRef" />
-              <TreeItem nodeId="14" label="useCallback" />
-            </TreeItem>
-            <TreeItem nodeId="15" label="react-router-dom" />
-            <TreeItem nodeId="16" label="styled-components" />
-            <TreeItem nodeId="17" label="Suspense" />
-            <TreeItem nodeId="18" label="OAuth" />
-            <TreeItem nodeId="19" label="Apollo-Client" />
+      <TreeItem nodeId="20" label="Front-End">
+        <TreeItem nodeId="8" label="README" />
+        <TreeItem nodeId="9" label="React">
+          <TreeItem nodeId="26" label="Environment" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+          <TreeItem nodeId="10" label="React Hooks">
+            <TreeItem nodeId="11" label="useState" />
+            <TreeItem nodeId="12" label="useEffect" />
+            <TreeItem nodeId="13" label="useRef" />
+            <TreeItem nodeId="14" label="useCallback" />
           </TreeItem>
-          <TreeItem nodeId="21" label="Vue">
-            <TreeItem nodeId="22" label="Vue v3.x" />
-          </TreeItem>
+          <TreeItem nodeId="15" label="react-router-dom" />
+          <TreeItem nodeId="16" label="styled-components" />
+          <TreeItem nodeId="17" label="Suspense" />
+          <TreeItem nodeId="18" label="OAuth" />
+          <TreeItem nodeId="19" label="Apollo-Client" />
         </TreeItem>
-        <TreeItem nodeId="23" label="Back-End">
-          <TreeItem nodeId="24" label="Node.js" />
-          <TreeItem nodeId="25" label="OpenAPI" />
+        <TreeItem nodeId="21" label="Vue">
+          <TreeItem nodeId="22" label="Vue v3.x" />
         </TreeItem>
       </TreeItem>
+      <TreeItem nodeId="23" label="Back-End">
+        <TreeItem nodeId="24" label="Node.js" />
+        <TreeItem nodeId="25" label="OpenAPI" />
+      </TreeItem>
+    </TreeItem>
     </>
   )
 }
