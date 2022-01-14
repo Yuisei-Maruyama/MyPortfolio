b558270416cee7aaaddac97174986bbed3c5cb9c
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-15 01:02:12 +0900

    update: Header自体の刷新

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Container } from '@material-ui/core'
 import { Main, Board, History, Art, Travel, Document } from '@/pages'
-import { Header, Footer } from '@/components'
+import { Header2, Footer, MarkdownPreviewer } from '@/components'
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
-        <Header></Header>
+        <Header2></Header2>
         <Container maxWidth="xl" style={{ paddingLeft: 0, paddingRight: 0 }}>
           <Switch>
             <Route exact path="/" component={Main} />
-            <Route path="/board" component={Board} />
-            <Route path="/history" component={History} />
-            <Route path="/art" component={Art} />
-            <Route path="/travel" component={Travel} />
-            <Route path="/document/:label" component={Document} />
+            <Route exact path="/board" component={Board} />
+            <Route exact path="/history" component={History} />
+            <Route exact path="/art" component={Art} />
+            <Route exact path="/travel" component={Travel} />
+              <Route path="/documents" component={Document} />
+              <Route path="/documents/:label" component={MarkdownPreviewer} />
           </Switch>
         </Container>
         <Footer />
