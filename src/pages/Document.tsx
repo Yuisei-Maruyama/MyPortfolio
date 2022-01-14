94acc9069eaa3dcc4709f3f457c34c04eb6ddc40
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-15 01:06:18 +0900

    update: Documentページのレイアウトの刷新

--- src/pages/Document.tsx
+++ src/pages/Document.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
-import { MarkdownPreviewer } from '@/components'
-import { useParams } from 'react-router-dom'
+import TreeView from '@mui/lab/TreeView'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import { DocumentList, MarkdownPreviewer } from '@/components'
+import { Box } from '@mui/material'
+import { useSetParams } from '@/customHooks'
 
-const Document = () => {
+const Document: React.FC = () => {
 
-  const params: { label: string } = useParams()
+  const { getParams, params } = useSetParams()
 
   return (
-    <div style={{ margin: "2% auto", width: "60%" }}>
-      <MarkdownPreviewer fileName={`${params.label}.md`} />
-    </div>
+    <Box sx={{ margin: "2% 4%", display: 'flex' }}>
+      <TreeView
+        aria-label="icon expansion"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{ height: 1200, flexGrow: 1, maxWidth: 400, overflowY: 'auto', marginRight: '10%' }}
+      >
+        <DocumentList getParams={getParams} />
+      </TreeView>
+      <div>
+        {
+          params
+            ? (
+              <Box sx={{ width: '80%'}}>
+                <MarkdownPreviewer fileName={`${params}.md`} />
+              </Box>
+            )
+            : <p>左のドキュメントリストを選択すると、ここにMarkdownがプレビューされます。</p>
+        }
+      </div>
+    </Box>
   )
 }
 
