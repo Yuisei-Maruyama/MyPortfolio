3f3b9a752d64031870a37e8fed87c564b759be7a
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-30 01:51:55 +0900

    add: FlippedCardの作成

---
+++ src/components/FlippedCard/FlippedCard.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+
+type Props = {
+  children?: React.ReactNodeArray
+  flipDirection?: 'horizontal' | 'vertical'
+  infinite: boolean
+  isFlipped: boolean
+  setFlipped: React.Dispatch<React.SetStateAction<boolean>>
+  setInfinite: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const FlippedCard: React.FC<Props> = (props: Props) => {
+
+  const { children, isFlipped, infinite } = props
+
+  const [rotation, setRotation] = useState(0)
+
+  const frontRotateX = `rotateX(${
+    infinite ? rotation : isFlipped ? 180 : 0
+    }deg)`;
+  const backRotateX = `rotateX(${
+    infinite ? rotation + 180 : isFlipped ? 0 : -180
+    }deg)`;
+
+  console.log(frontRotateX)
+  console.log(backRotateX)
+
+  console.log(children)
+
+  useEffect(() => {
+    if (isFlipped) {
+      setRotation((c) => c + 180);
+    }
+  }, [isFlipped])
+
+  return (
+    <>
+      {
+        Array.isArray(children) && children?.length === 2
+          ? (
+            <>
+              <div key={0}>{ children[0] }</div>
+              <div key={1}>{ children[1] }</div>
+            </>
+          )
+          : <p>FlippedCard need two elements!!</p>
+      }
+    </>
+  )
+}
+
+export default FlippedCard
