1febd47498c58c9f6b5e61c86294a6b9fc033754
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-01-30 23:17:12 +0900

    update: 部分修正

--- src/components/FlippedCard/FlippedCard.tsx
+++ src/components/FlippedCard/FlippedCard.tsx
@@ -1,41 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, memo } from 'react'
 
 type Props = {
   children?: React.ReactNodeArray
   flipDirection?: 'horizontal' | 'vertical'
-  infinite: boolean
   isFlipped: boolean
-  setFlipped: React.Dispatch<React.SetStateAction<boolean>>
-  setInfinite: React.Dispatch<React.SetStateAction<boolean>>
+  setFlipped: (isFlipped: boolean) => void
 }
 
 const FlippedCard: React.FC<Props> = (props: Props) => {
 
-  const { children, isFlipped, infinite, setFlipped } = props
+  const { children, isFlipped, setFlipped } = props
 
   const [rotation, setRotation] = useState(0)
 
-  const frontRotateX = `rotateX(${
-    infinite ? rotation : isFlipped ? 180 : 0
-    }deg)`;
-  const backRotateX = `rotateX(${
-    infinite ? rotation + 180 : isFlipped ? 0 : -180
-    }deg)`;
-
-  console.log(frontRotateX)
-  console.log(backRotateX)
-
-  console.log(children)
-
   const handleHover = () => {
+    if (isFlipped) {
+      setRotation((c) => c + 180)
+    }
+    setRotation((c) => c - 180)
     setFlipped(!isFlipped)
   }
-
-  useEffect(() => {
-    if (isFlipped) {
-      setRotation((c) => c + 180);
-    }
-  }, [isFlipped])
 
   return (
     <>
@@ -43,7 +27,11 @@ const FlippedCard: React.FC<Props> = (props: Props) => {
         Array.isArray(children) && children?.length === 2
           ? (
             <>
-              { !isFlipped ? <div onMouseEnter={handleHover}>{ children[0] }</div> : <div onMouseLeave={handleHover}>{ children[1] }</div>  }
+              {
+                !isFlipped
+                  ? <div style={{ transform: `rotateY(${rotation})`}} onMouseEnter={handleHover}>{ children[0] }</div>
+                  : <div style={{ transform: `rotateY(${rotation})`}} onMouseLeave={handleHover}>{ children[1] }</div>
+              }
             </>
           )
           : <p>FlippedCard need two elements!!</p>
@@ -52,4 +40,4 @@ const FlippedCard: React.FC<Props> = (props: Props) => {
   )
 }
 
-export default FlippedCard
+export default memo(FlippedCard)
