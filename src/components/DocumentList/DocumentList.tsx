3acb9f673948e12b9325b32921bb7b8e0cab3408
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-02-01 01:44:20 +0900

    update: DocumentListの修正

--- src/components/DocumentList/DocumentList.tsx
+++ src/components/DocumentList/DocumentList.tsx
@@ -31,38 +31,43 @@ const DocumentList: React.FC<Props> = (props: Props) => {
           <TreeItem nodeId="2" label="README" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
           <TreeItem nodeId="3" label="React">
             <TreeItem nodeId="4" label="Environment" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+            <TreeItem nodeId="99" label="Basics" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+            <TreeItem nodeId="100" label="PerformanceTuning" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
             <TreeItem nodeId="5" label="Props(React)" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
             <TreeItem nodeId="6" label="React Hooks">
-              <TreeItem nodeId="7" label="useState" />
+              <TreeItem nodeId="7" label="useState" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
               <TreeItem nodeId="8" label="useEffect" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
-              <TreeItem nodeId="9" label="useRef" />
-              <TreeItem nodeId="10" label="useCallback" />
+              <TreeItem nodeId="9" label="useContext" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+              <TreeItem nodeId="10" label="useReducer" />
+              <TreeItem nodeId="11" label="useRef" />
+              <TreeItem nodeId="12" label="useCallback" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+              <TreeItem nodeId="13" label="useMemo" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
             </TreeItem>
-            <TreeItem nodeId="11" label="react-router-dom" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
-            <TreeItem nodeId="12" label="styled-components" />
-            <TreeItem nodeId="13" label="Suspense" />
-            <TreeItem nodeId="14" label="OAuth" />
-            <TreeItem nodeId="15" label="Apollo-Client" />
-            <TreeItem nodeId="21" label="MaterialUI" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+            <TreeItem nodeId="14" label="react-router-dom" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+            <TreeItem nodeId="15" label="styled-components" />
+            <TreeItem nodeId="16" label="Suspense" />
+            <TreeItem nodeId="17" label="OAuth" />
+            <TreeItem nodeId="18" label="Apollo-Client" />
+            <TreeItem nodeId="19" label="MaterialUI" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
           </TreeItem>
-          <TreeItem nodeId="16" label="Vue">
-            <TreeItem nodeId="17" label="CompositionAPI">
-              <TreeItem nodeId="18" label="Props(Vue)" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
-              <TreeItem nodeId="19" label="Emit" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
-              <TreeItem nodeId="20" label="Two-Way Binding" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+          <TreeItem nodeId="20" label="Vue">
+            <TreeItem nodeId="21" label="CompositionAPI">
+              <TreeItem nodeId="22" label="Props(Vue)" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+              <TreeItem nodeId="23" label="Emit" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+              <TreeItem nodeId="24" label="Two-Way Binding" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
             </TreeItem>
           </TreeItem>
         </TreeItem>
       </Box>
       <Box sx={{ marginTop: '10%'}}>
-        <TreeItem nodeId="22" label="Back-End">
-          <TreeItem nodeId="23" label="Node.js" />
-          <TreeItem nodeId="24" label="OpenAPI" />
+        <TreeItem nodeId="25" label="Back-End">
+          <TreeItem nodeId="26" label="Node.js" />
+          <TreeItem nodeId="27" label="OpenAPI" />
         </TreeItem>
       </Box>
       <Box sx={{ marginTop: '10%'}}>
-        <TreeItem nodeId="25" label="TypeScript">
-        <TreeItem nodeId="16" label="types-cheat-sheet" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
+        <TreeItem nodeId="28" label="TypeScript">
+        <TreeItem nodeId="29" label="types-cheat-sheet" onClick={(e: React.MouseEvent<HTMLElement, MouseEvent>) => previewDocument(e)} />
         </TreeItem>
       </Box>
     </>
