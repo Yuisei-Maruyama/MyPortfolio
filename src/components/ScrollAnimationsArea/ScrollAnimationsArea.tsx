d4feed39ed029608c17bf98709d8ec0ef838c226
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-03-02 00:30:22 +0900

    add: ScrollAnimationsAreaコンポーネントの作成

---
+++ src/components/ScrollAnimationsArea/ScrollAnimationsArea.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Parallax } from 'react-scroll-parallax'
+
+const ScrollAnimationsArea: React.FC = () => {
+  return (
+    <Parallax speed={-10}>
+      <h1>Parallax</h1>
+    </Parallax>
+  )
+}
+
+export default ScrollAnimationsArea
