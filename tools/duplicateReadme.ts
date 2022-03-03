b316b884af9aa8b92890b7d1cc6869c51a901356
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-03-04 03:11:14 +0900

    update: ドキュメント配下のREADMEに対して、ルート配下のREADMEで上書きする

--- tools/duplicateReadme.ts
+++ tools/duplicateReadme.ts
@@ -1,5 +1,14 @@
-import { readFileSync } from 'fs'
+import { readFileSync, statSync, writeFileSync } from 'fs'
 
 const readme = readFileSync('README.md', 'utf8')
 
-console.log(readme)
+try {
+  const existReadme = statSync('documents/README.md')
+  if (existReadme.isFile()) {
+    writeFileSync('documents/README.md', readme, 'utf8')
+  }
+} catch (err) {
+  console.log(err)
+}
+
+
