8d51803a0715716ad89597fea321f0a10f24227f
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-03-05 02:19:35 +0900

    update: profileカードの高さ調整

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -52,8 +52,8 @@ const Main = () => {
       <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 15 }}>
         <Box sx={{ width: '20%' }}>
           <FlippedCard isFlipped={isFlipped} setFlipped={handleSetFlipped}>
-            <ProfileFrontCard height="400px" />
-            <ProfileBackCard height="400px" />
+            <ProfileFrontCard height="450px" />
+            <ProfileBackCard height="450px" />
           </FlippedCard>
         </Box>
         <Box sx={{ ml: 12, width: '40%' }}>
