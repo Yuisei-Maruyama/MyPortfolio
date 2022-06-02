6f05cb0a1e0aa76748914fdafbe91a6475f0a0af
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-06-03 01:11:54 +0900

    add: ProgressBarコンポーネント

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from '@emotion/styled';
+
+type Props = {
+  style: Record<string, string | number>
+}
+
+const ProgressBar: React.FC<Props> = ({style}) => {
+
+  const _Progress = styled.div`
+    width: 75%;
+    height: 50px;
+    margin: 0 auto;
+    border: 1px solid #06D8D7;
+    background-color: '#fff';
+    border-radious: 50px !important;
+  `
+
+  const $PropsProgress = styled(_Progress)`
+    ${style}
+  `
+
+  return (
+    <div>
+      <$PropsProgress />
+    </div>
+  );
+}
+
+export default ProgressBar;
