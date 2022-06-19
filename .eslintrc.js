9efe2da4cefc6f0244e37396027e1d26bb1f37f7
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-06-19 22:56:22 +0900

    update: import整形の追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,12 +22,14 @@ module.exports = {
     'react',
     '@typescript-eslint',
     'react-hooks',
-    'styled-components-varname'
+    'styled-components-varname',
+    "import"
   ],
   rules: {
     'no-use-before-define': 'off', // import React from 'react' のエラー回避
     '@typescript-eslint/no-use-before-define': ['error', { "variables": false }], // styled-componentsの定義場所を.tsx下部に設定するため
     'camelCase': 'off',
+    "import/no-duplicates": "error",
     'space-before-function-paren': 'off',
     'react/react-in-jsx-scope': 'off',
     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
