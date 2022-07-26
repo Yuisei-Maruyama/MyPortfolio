e46fcbc16c432f11d9ff16bed7b0505ce69de7d8
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-07-27 01:21:57 +0900

    update: Tableにページネーション機能を付与する

--- src/components/SelectableTable/SelectableTable.tsx
+++ src/components/SelectableTable/SelectableTable.tsx
@@ -1,5 +1,5 @@
-import { FC, ReactNode } from 'react'
-import { TableContainer, Paper, Table, TableBody, TableRow, TableCell, Checkbox, Box, Typography } from '@mui/material'
+import React, { FC, ReactNode, useState } from 'react'
+import { TableContainer, Paper, Table, TableBody, TableRow, TableCell, TablePagination, Checkbox, Box, Typography } from '@mui/material'
 import SelectableTableHead from './SelectableTableHead'
 import { useTableRowSelect } from '@/customHooks'
 
@@ -20,6 +20,19 @@ type Props = {
 }
 
 const SelectableTable: FC<Props> = ({ tableContents, tableHeadCells, children }) => {
+
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(5);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   const {
     selectedRowIds,
     getSelectedRows,
@@ -32,39 +45,50 @@ const SelectableTable: FC<Props> = ({ tableContents, tableHeadCells, children })
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table>
-          <SelectableTableHead
-            onSelectAllClick={toggleSelectedAll}
-            tableHeadCells={tableHeadCells}
-            checked={isSelectedAll}
-            indeterminate={isIndeterminate}
-          />
-          <TableBody>
-            {tableContents.map((tableContent) => {
-              const isItemSelected = isSelectedIds(tableContent.id)
+      <Paper sx={{ width: '100%' }}>
+        <TableContainer component={Paper}>
+          <Table>
+            <SelectableTableHead
+              onSelectAllClick={toggleSelectedAll}
+              tableHeadCells={tableHeadCells}
+              checked={isSelectedAll}
+              indeterminate={isIndeterminate}
+            />
+            <TableBody>
+              {tableContents.map((tableContent) => {
+                const isItemSelected = isSelectedIds(tableContent.id)
 
-              return (
-                <TableRow
-                  hover
-                  role="checkbox"
-                  tabIndex={-1}
-                  key={tableContent.id}
-                  onClick={() => toggleSelected(tableContent.id)}
-                  selected={isItemSelected}
-                >
-                  <TableCell padding="checkbox">
-                    <Checkbox checked={isItemSelected} />
-                  </TableCell>
-                  {Object.values(tableContent).map((tableContentKey) => {
-                    return <TableCell key={tableContentKey}>{tableContentKey}</TableCell>
-                  })}
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={tableContent.id}
+                    onClick={() => toggleSelected(tableContent.id)}
+                    selected={isItemSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox checked={isItemSelected} />
+                    </TableCell>
+                    {Object.values(tableContent).map((tableContentKey) => {
+                      return <TableCell key={tableContentKey}>{tableContentKey}</TableCell>
+                    })}
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25, 100]}
+          component="div"
+          count={tableContents.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
       {children ? (
         <>
           <Box sx={{ marginTop: '25px' }}>
