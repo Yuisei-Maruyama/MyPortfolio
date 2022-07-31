202479c942decc97b09b8fbb8fa54f7aa2e43cd8
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-07-31 16:50:18 +0900

    update: CustomHook名称の変更

--- src/components/DocumentListTree/DocumentListTree.tsx
+++ src/components/DocumentListTree/DocumentListTree.tsx
@@ -4,27 +4,25 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import { ducumentList } from '@/data/ducumentList'
 import { DocumentItem } from '@/types'
-import { useShowDocument } from '@/customHooks/'
+import { useShowTree } from '@/customHooks/'
 
 type Props = {
   getParams: (params: string) => void
 }
 
 const DocumentListTree: React.FC<Props> = ({ getParams }) => {
+  const { previewTree } = useShowTree(getParams)
 
-  const { previewDocument } = useShowDocument(getParams)
+  const documentListData = ducumentList(previewTree)
 
-  const documentListData = ducumentList(previewDocument)
-
-  const renderTree = (nodesList: DocumentItem[]) => (
-    nodesList.map(nodes => {
+  const renderTree = (nodesList: DocumentItem[]) =>
+    nodesList.map((nodes) => {
       return (
         <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.label} onClick={nodes.onClick}>
           {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree([node])) : null}
         </TreeItem>
       )
     })
-  );
 
   return (
     <TreeView
@@ -36,7 +34,7 @@ const DocumentListTree: React.FC<Props> = ({ getParams }) => {
     >
       {renderTree(documentListData)}
     </TreeView>
-  );
+  )
 }
 
 export default DocumentListTree
